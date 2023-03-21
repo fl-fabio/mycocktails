2e581b564a8fb62d765b35a2c5351c099c6fc6bb
Fabio Lanzafame <silver@MBP-DI-FABIO.station>
2023-03-21 17:16:13 +0100

    added components in main module card-list and card-detail

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardListComponent,
+    CardDetailComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
